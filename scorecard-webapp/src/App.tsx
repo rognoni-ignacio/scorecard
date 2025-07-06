1cd8d98f66417133370e0ce96853a9811f3a83ee
Ignacio ROGNONI <IRO@softfluent.com>
2025-07-06 13:37:39 +0200

    Add hole selection buttons

--- scorecard-webapp/src/App.tsx
+++ scorecard-webapp/src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [holes, setHoles] = useState<number>(18);
+
   return (
     <div>
       <h1>Golf Scorecard</h1>
+      <div>
+        <h2>How many holes are you playing today?</h2>
+
+        <div>
+          <button onClick={() => setHoles(9)}>9 holes</button>
+          <button onClick={() => setHoles(18)}>18 holes</button>
+        </div>
+
+        <p>Great! You are playing {holes} holes today.</p>
+
+      </div>
       <button>Start Game</button>
     </div>
   );
