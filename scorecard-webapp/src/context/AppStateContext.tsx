34574c2bf26470d6dc9a28ed8463cce05e42b156
Ignacio ROGNONI <IRO@softfluent.com>
2025-07-08 17:46:01 +0200

    Refactor AppStateContext to remove unused type definition for AppState

--- scorecard-webapp/src/context/AppStateContext.tsx
+++ scorecard-webapp/src/context/AppStateContext.tsx
@@ -2,11 +2,6 @@ import type { ReactNode } from "react";
 import { useState } from "react";
 import { AppStateContext } from "./context";
 
-type AppState = {
-  holes: number | null;
-  setHoles: (holes: number) => void;
-};
-
 export function AppStateProvider({ children }: { children: ReactNode }) {
   const [holes, setHoles] = useState<number | null>(null);
 
