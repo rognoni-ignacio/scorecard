0f20c4a05f9cc058843cc7e353df5e10b7d0992d
Ignacio ROGNONI <IRO@softfluent.com>
2025-07-25 15:12:11 +0200

    Once again, try vercel config

--- scorecard-api/api/index.py
+++ scorecard-api/api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from .courses import courses_bp
 
@@ -6,6 +6,3 @@ app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173", "https://rognoni-ignacio.github.io"])
 
 app.register_blueprint(courses_bp)
-
-if __name__ == "main":
-    app.run()
